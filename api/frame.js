ad156a31a8430b06025da28f93f2b89a1257f2b6
seosan456-cmyk <seosan456@gmail.com>
2026-09-21 16:24:51 +0900

    Adjust event row spacing

--- api/frame.js
+++ api/frame.js
@@ -749,9 +749,9 @@ function eventsSvg(events) {
         return `
           <line
             x1="24"
-            y1="${top + 25}"
+            y1="${top + 22}"
             x2="456"
-            y2="${top + 25}"
+            y2="${top + 22}"
             class="softLine"
           />
 
